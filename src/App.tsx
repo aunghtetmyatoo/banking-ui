d1bb600018581a6e7ec2392b73883d451f4592cc
ahmo <aunghtetmyatoo888@gmail.com>
2024-09-26 23:48:43 +0630

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import Header from "./components/Header";
 import "./App.css";
+import State from "./components/State";
 
 export default function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="mt-10 px-40">
+        <State />
+      </div>
+    </>
+  );
 }
